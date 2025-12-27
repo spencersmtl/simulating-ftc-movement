4e6ce721084ab66965cbabaf65660a4f0a82cba1
spencersmtl <65173735+spencersmtl@users.noreply.github.com>
2025-12-27 12:45:17 -0400

    wrapped distance working
    Horizontal wrapping now working for neighbor and distance calculations. Using naive square window for neighbor searching for now - should improve this to true hex neighbors later.
    Added habitat preference (beta, Q) to initialize_dispersal. Updated small_tester.R to use new parameters, run for more timesteps, and demonstrate habitat preference effects.

--- code/small_tester.R
+++ code/small_tester.R
@@ -1,47 +1,45 @@
 source("code/mapping.R")
 source("code/model.R")
 {
-  timesteps <- 2
+  timesteps <- 20
+  max_host_dispersal <- 2 # number of adjacent cells
+  beta <- 0.8 # habitat preference
   
   # load landscape
-  landscape <- load_landscape(
-    "images/blobs.png",
-    scale = 0.5, # Set scale <1 to shrink image
-    cellsize = 80 # set cellsize <8 to make a finer hex grid overlay
-  )
-  landscape <- basic_habitat_quality( # set habitat quality threshold
-    landscape, 
-    threshold = 0.5
-  ) 
+  landscape <- load_landscape("images/blobs.png", scale = 0.5, # Set scale <1 to shrink image
+    cellsize = 80) # set cellsize <8 to make a finer hex grid overlay
+  landscape <- basic_habitat_quality(landscape, threshold = 0.5) # set habitat quality threshold
+  landscape$Q <- ifelse(landscape$type == "high", 1, 0)
   
   # Compute neighbours
-  neighbs <- compute_neighbors(landscape, max_host_dispersal = 1)
+  neighbs <- compute_neighbors(landscape, max_host_dispersal)
   
   # Compute neighbour distances
-  dists <- compute_sparse_distance(landscape, neighbs, max_host_dispersal = 1)
-  
-  # Generate sparse distance matrix
-  D <- compute_sparse_distance(landscape, max_host_dispersal = 1)
+  dists <- compute_sparse_distance(landscape, neighbs, max_host_dispersal)
   
   # Generate dispersal kernel
   K <- initialize_dispersal(
-    D, 
-    stay_prob = 0.5,
-    scale = 1, 
-    kernel_function = "negative_exp"
+    dists, # sparse distance matrix
+    stay_prob = 0.5, # probability of staying in cell
+    scale = 1, # drop-off scale
+    kernel_function = "negative_exp",
+    beta, # habitat preference strength
+    Q = landscape$Q # habitat quality vector
   )
   
   # Initialize density
   H <- matrix(NA_real_, nrow = nrow(landscape), ncol = timesteps)
-  H[,1] <- c(0,0,0,1,0,0,0,0,0) # initial host population in cell 4
+  H[,1] <- 1
+  H[,1] <- H[,1] / sum(H[,1])
   
-  # Disperse once
-  H[,2] <- disperse( 
-    kernel = K, 
-    density = H[,1], 
-    survival = 1 
-  )
+  # Disperse
+  for (i in 2:timesteps) {
+    H[,i] <- disperse(
+      kernel = K,
+      density = H[,i-1],
+      survival = 1
+    )
+  }
   
-  visualise_landscape(landscape, density = H[,2])
+  visualise_landscape(landscape, density = H[,20])
 }
-#   row_sums <- rowSums(K)
